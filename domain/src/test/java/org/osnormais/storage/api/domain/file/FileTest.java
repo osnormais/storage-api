932c7154e00ffe970f5a23bac93e76a6b473dd08
livia1111 <livia1111@gmail.com>
2026-03-02 20:15:12 -0300

    corrige teste caso Id é nulo

--- domain/src/test/java/org/osnormais/storage/api/domain/file/FileTest.java
+++ domain/src/test/java/org/osnormais/storage/api/domain/file/FileTest.java
@@ -1,6 +1,7 @@
 package org.osnormais.storage.api.domain.file;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.UUID;
 
@@ -38,12 +39,9 @@ public class FileTest {
         assertEquals(expectedErrorMessage, actualErrors.get(0).message());
 
     }
+
     @Test
     void givenNullId_whenValidate_thenHandlerShouldAppendError() {
-
-        final var expectedErrorsCount = 1;
-        final var expectedHasErrors = true;
-        final var expectedErrorMessage = "id cant be null";
 
         final FileId expectedFileId = null;
         final var expectedSize = 2L;
@@ -52,20 +50,12 @@ public class FileTest {
         final var expectedChecksumValue = "123";
         final var expectedChecksum = new Checksum(expectedChecksumAlgorithm, expectedChecksumValue);
 
-        final var expectedFile = new File(expectedFileId, expectedSize, expectedChecksum);
+        final var actualException = assertThrows(NullPointerException.class,
+                () -> new File(expectedFileId, expectedSize, expectedChecksum));
+        assertEquals("'id' should not be null", actualException.getMessage());
 
-        final var handler = Notification.create();
-
-        expectedFile.validate(handler);
-
-        final var actualHasErrors = handler.hasErrors();
-        final var actualErrors = handler.getErrors();
-
-        assertEquals(expectedErrorsCount, actualErrors.size());
-        assertEquals(expectedHasErrors, actualHasErrors);
-        assertEquals(expectedErrorMessage, actualErrors.get(0).message());
     }
-    
+
     @Test
     void givenNullChecksum_whenValidate_thenHandlerShouldAppendError() {
 
@@ -92,11 +82,11 @@ public class FileTest {
         assertEquals(expectedHasErrors, actualHasErrors);
         assertEquals(expectedErrorMessage, actualErrors.get(0).message());
     }
+
     @Test
-    void givenValidFile_whenValidate_thenHandlerShouldNotAppendError() { 
+    void givenValidFile_whenValidate_thenHandlerShouldNotAppendError() {
         final var expectedErrorsCount = 0;
         final var expectedHasErrors = false;
-        final var expectedErrorMessage = "";
 
         final var expectedIdValue = UUID.randomUUID();
         final var expectedFileId = new FileId(expectedIdValue);
@@ -117,8 +107,6 @@ public class FileTest {
 
         assertEquals(expectedErrorsCount, actualErrors.size());
         assertEquals(expectedHasErrors, actualHasErrors);
-        assertEquals(expectedErrorMessage, actualErrors.get(0).message());
-
 
     }
 }
