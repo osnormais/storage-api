a0a531323d1dfe545ed98c6641c6a3177b288a96
jhonatapers <jhonatapers@gmail.com>
2026-03-05 13:01:34 -0300

    feat: adiciona validação em ChunkSpecification

--- domain/src/main/java/org/osnormais/storage/api/domain/file/valueobject/ChunkSpecification.java
+++ domain/src/main/java/org/osnormais/storage/api/domain/file/valueobject/ChunkSpecification.java
@@ -1,10 +1,29 @@
 package org.osnormais.storage.api.domain.file.valueobject;
 
+import static java.util.Objects.isNull;
+
 import org.osnormais.storage.api.domain.ValueObject;
+import org.osnormais.storage.api.domain.validation.ValidationError;
+import org.osnormais.storage.api.domain.validation.handler.ValidationHandler;
 
 public record ChunkSpecification(
         Size size,
         ParallelChunkLimit maxParallel) implements ValueObject {
+
+    @Override
+    public void validate(final ValidationHandler handler) {
+
+        if (isNull(size))
+            handler.append(ValidationError.with("Chunk 'size' should not be null"));
+        else
+            size.validate(handler);
+
+        if (isNull(maxParallel))
+            handler.append(ValidationError.with("Chunk 'maxParallel' should not be null"));
+        else
+            maxParallel.validate(handler);
+
+    }
 
     public Size lastChunkSize(final Size fileSize) {
 
