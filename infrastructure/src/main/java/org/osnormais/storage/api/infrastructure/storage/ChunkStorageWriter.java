b7f287247c111b9a135de8f305546a1ec4d00e9f
jhonatapers <jhonatapers@gmail.com>
2026-03-15 15:53:19 -0300

    feat: implementa ChunkStorageWriter para escrita de chunks de arquivos

---
+++ infrastructure/src/main/java/org/osnormais/storage/api/infrastructure/storage/ChunkStorageWriter.java
@@ -0,0 +1,45 @@
+package org.osnormais.storage.api.infrastructure.storage;
+
+import java.io.InputStream;
+import java.util.Objects;
+
+import org.osnormais.storage.api.application.port.ChunkWriter;
+import org.osnormais.storage.api.domain.file.FileId;
+import org.osnormais.storage.api.domain.file.valueobject.Checksum;
+import org.osnormais.storage.api.domain.file.valueobject.Size;
+import org.osnormais.storage.api.domain.file.valueobject.ThroughputLimit;
+
+public class ChunkStorageWriter implements ChunkWriter {
+
+    private final StorageWriter storageWriter;
+
+    public ChunkStorageWriter(final StorageWriter storageWriter) {
+        this.storageWriter = Objects.requireNonNull(storageWriter);
+    }
+
+    @Override
+    public Checksum writeChunk(
+            FileId key,
+            Long chunkIndex,
+            Size chunkSize,
+            ThroughputLimit throughputLimit,
+            Checksum.Algorithm checksumAlgorithm,
+            InputStream inputStream) {
+
+        final StorageKey chunkStorageKey = StorageKey
+                .create("files", key.getStringValue())
+                .subKey(
+                        "upload",
+                        "chunks",
+                        chunkIndex.toString());
+
+        return storageWriter.write(
+                chunkStorageKey,
+                inputStream,
+                chunkSize.bytes(),
+                throughputLimit.bytesPerSecond(),
+                checksumAlgorithm);
+
+    }
+
+}
