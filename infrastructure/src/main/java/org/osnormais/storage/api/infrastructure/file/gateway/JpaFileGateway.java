b7afc62a51fcf8fe56caf72a67a953a6794546af
jhonatapers <jhonatapers@gmail.com>
2026-03-28 17:15:50 -0300

    feat: implementa JpaFileGateway

---
+++ infrastructure/src/main/java/org/osnormais/storage/api/infrastructure/file/gateway/JpaFileGateway.java
@@ -0,0 +1,59 @@
+package org.osnormais.storage.api.infrastructure.file.gateway;
+
+import java.util.Optional;
+
+import org.osnormais.storage.api.application.gateway.file.FileCommandGateway;
+import org.osnormais.storage.api.application.gateway.file.FileQueryGateway;
+import org.osnormais.storage.api.domain.file.File;
+import org.osnormais.storage.api.domain.file.FileId;
+import org.osnormais.storage.api.infrastructure.file.persistence.FileJpaEntity;
+import org.osnormais.storage.api.infrastructure.file.persistence.FileJpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class JpaFileGateway implements FileCommandGateway, FileQueryGateway {
+
+    private final FileJpaRepository fileRepository;
+
+    public JpaFileGateway(final FileJpaRepository fileRepository) {
+        this.fileRepository = fileRepository;
+    }
+
+    @Override
+    public Optional<File> findById(final FileId id) {
+        return fileRepository
+                .findById(id.getValue())
+                .map(FileJpaEntity::toDomain);
+    }
+
+    @Transactional
+    @Override
+    public File create(final File file) {
+
+        if (fileRepository.existsById(file.getId().getValue()))
+            throw new IllegalStateException("File with id %s already exists".formatted(file.getId().getStringValue()));
+
+        save(file);
+
+        return file;
+    }
+
+    @Transactional
+    @Override
+    public File update(final File file) {
+
+        if (!fileRepository.existsById(file.getId().getValue()))
+            throw new IllegalStateException("File with id %s does not exist".formatted(file.getId().getStringValue()));
+
+        save(file);
+
+        return file;
+
+    }
+
+    private void save(final File file) {
+        fileRepository.save(FileJpaEntity.fromDomain(file));
+    }
+
+}
